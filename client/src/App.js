348eb7546a3313f050021b9f2dc6909353e553ed
Joice Mota <81887835+Joicemota99@users.noreply.github.com>
2022-11-11 22:08:51 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home/index.jsx";
 import { Autor } from "./pages/cadastroAutor/index.jsx";
 import { Livro } from "./pages/cadastroLivro/index.jsx";
-import { Pesquisa } from "./pages/pesquisar/index.jsx";
-import { Todos } from "./pages/todosLivros/index.jsx";
+
 
 function App() {
   return (
